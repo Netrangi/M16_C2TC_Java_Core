a422a0ccc7eabfcbebc317a6d895f9727ad01657
Netra <Netra@DESKTOP-DSM5VNG>
2022-04-14 20:42:39 +0530

    First Commit

---
+++ Day2_Intro_to_Java/src/in/cg/java/Example1.java
@@ -0,0 +1,20 @@
+package in.cg.java;
+
+public class Example1 {
+
+	public static void main(String[] args)
+	{
+		int a=10;//whole
+		float b=10.5f;//decimal point
+		double c=14.7;//decimal point
+		char d='x';
+		String str="Netrangi Jayant Joshi";
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(c);
+		System.out.println(d);
+		System.out.println(str);
+
+	}
+
+}
