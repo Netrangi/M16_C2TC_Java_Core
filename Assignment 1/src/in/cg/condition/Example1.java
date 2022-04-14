f0e2299be163ee19bff60d6f8d70080e792da15b
Netra <Netra@DESKTOP-DSM5VNG>
2022-04-14 21:06:26 +0530

    First Commit

---
+++ Assignment 1/src/in/cg/condition/Example1.java
@@ -0,0 +1,19 @@
+package in.cg.condition;
+
+public class Example1 {
+
+	public static void main(String[] args) 
+	{
+		int d = 12,c= 10;
+		if (d>c)
+		{
+			System.out.println("d is greater than c");
+		}
+		else
+		{
+			System.out.println("c is greater than d");
+		}
+
+	}
+
+}
---
+++ Assignment 1/src/in/cg/condition/Example3.java
@@ -0,0 +1,29 @@
+package in.cg.condition;
+
+public class Example3 
+{
+
+	public static void main(String[] args)
+	{
+		int age =15,weight = 55;
+		if (age >12)
+		{
+			if (weight >=40) 
+			{
+				if (weight <=110)
+				{
+					System.out.println("Eligible for bunjee jumping");
+				}
+				else 
+				{
+					System.out.println("extra rope will be added");
+				}
+			}
+			else
+			{
+				System.out.println(" you");
+			}
+
+		}
+	}
+}
