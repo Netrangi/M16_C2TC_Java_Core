a2e3d166733e6d94df0f340ff491bedac0acafeb
Netra <Netra@DESKTOP-DSM5VNG>
2022-04-14 20:53:55 +0530

    First Commit

---
+++ Day3_OOPs/src/in/cg/oops/Car.java
@@ -0,0 +1,17 @@
+package in.cg.oops;
+
+public class Car 
+{
+	String name="BMW";//variable
+	void print()//method
+	{
+		System.out.println("This is "+name);
+	}
+	public static void main(String[] args) 
+	{
+		Car obj1=new Car();
+		obj1.print();
+
+	}
+
+}
---
+++ Day3_OOPs/src/in/cg/oops/Example1.java
@@ -0,0 +1,15 @@
+package in.cg.oops;
+
+public class Example1
+{
+	Example1()
+	{
+		System.out.println("Welcome to TNS Program in JAVA of Capgemini");
+	}
+	public static void main(String[] args)
+	{
+		Example1 e=new Example1();
+
+	}
+
+}
