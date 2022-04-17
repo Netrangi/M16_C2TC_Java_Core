acc47b0ff6bde8dd1758d66f8d8b995f31fc3b56
Netra <Netra@DESKTOP-DSM5VNG>
2022-04-17 18:41:53 +0530

    First Commit

---
+++ Day7_this_static_instanceof/src/in/cg/this1/Example5.java
@@ -0,0 +1,24 @@
+package in.cg.this1;
+
+public class Example5 {
+	
+	int x;
+	Example5()
+	{
+		this(25);
+		System.out.println("Netrangi Joshi");
+	}
+	Example5(int x)
+	{
+		this.x=x;
+		System.out.println("Sanskar Joshi");
+	}
+	
+
+	public static void main(String[] args) {
+		Example5 e=new Example5();
+
+
+	}
+
+}
