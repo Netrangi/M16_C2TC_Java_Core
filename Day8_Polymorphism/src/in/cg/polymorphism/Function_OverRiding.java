fc9085bc18e4684fd9ddc52302540061565875ae
Netra <Netra@DESKTOP-DSM5VNG>
2022-04-18 16:08:52 +0530

    First Commit

--- Day8_Polymorphism/src/in/cg/polymorphism/Function_OverRiding.java
+++ Day8_Polymorphism/src/in/cg/polymorphism/Function_OverRiding.java
@@ -1,9 +1,39 @@
 package in.cg.polymorphism;
 
-public class Function_OverRiding {
+class Android
+{
+	void display()
+	{
+		System.out.println("Realme");
+	}
+}
+class Apple extends Android
+{
+	void  display()
+	{
+		System.out.println("iPhone 11 pro max");
+		
+	}
+}
+class Nokia extends Android
+{
+	void display()
+	{
+		System.out.println("iPhone");
+	}
+}
+
+public class Function_OverRiding
+{
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Apple a=new Apple();
+		Android a1=new Android();
+		Nokia a2=new Nokia();
+		a.display();
+		a1.display();
+		a2.display();
+		
 
 	}
 
