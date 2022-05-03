e198a26eebe0e9c9576e4b45676be68f0bbc48e8
Netra <Netra@DESKTOP-DSM5VNG>
2022-05-03 12:12:40 +0530

    First Commit

---
+++ Day15_Collection1.1_ArrayList_Linked_list/src/in/cg/collections/Example5.java
@@ -0,0 +1,15 @@
+package in.cg.collections;
+
+import java.util.Vector;
+
+public class Example5 {
+
+	public static void main(String[] args) {
+		Vector v= new Vector();
+		v.add("NETRANGI");
+		v.add(14);
+		System.out.println(v);
+
+	}
+
+}
---
+++ Day15_Collection1.1_ArrayList_Linked_list/src/in/cg/collections/Example6.java
@@ -0,0 +1,18 @@
+package in.cg.collections;
+
+import java.util.Stack;
+
+public class Example6 {
+
+	public static void main(String[] args) {
+		Stack s= new Stack();
+		s.add(10);
+		s.add(20);
+		s.pop();
+		System.out.println(s);
+		
+				
+
+	}
+
+}
