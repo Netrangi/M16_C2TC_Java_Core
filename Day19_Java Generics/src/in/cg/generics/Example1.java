4765ece28b9e7ebdf53c6ca1eb04189e7b8684d3
Netra <Netra@DESKTOP-DSM5VNG>
2022-05-13 13:15:11 +0530

    First Commit

---
+++ Day19_Java Generics/src/in/cg/generics/Example1.java
@@ -0,0 +1,16 @@
+package in.cg.generics;
+
+import java.util.ArrayList;
+public class Example1 {
+
+	public static void main(String[] args) {
+		ArrayList<String>  a=new ArrayList<String>();
+		a.add("1");
+		a.add("22");
+		a.add("5.5f");
+		
+		System.out.println(a);
+
+	}
+
+}
