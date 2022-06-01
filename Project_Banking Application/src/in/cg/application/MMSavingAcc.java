824444dd085c2714786f4680baa16222d4133d04
Netra <Netra@DESKTOP-DSM5VNG>
2022-06-01 14:53:24 +0530

    First Commit

---
+++ Project_Banking Application/src/in/cg/application/MMSavingAcc.java
@@ -0,0 +1,17 @@
+package in.cg.application;
+
+import in.cg.framework.SavingAcc;
+
+public class MMSavingAcc extends SavingAcc
+{
+
+	public MMSavingAcc(int accNo, String accNm, float accBal, boolean isSalaried) {
+		super(accNo, accNm, accBal, isSalaried);
+		
+	}
+
+	@Override
+	public String toString() {
+		return String.format("MMSavingAcc [toString()=%s]", super.toString());
+	}
+}
